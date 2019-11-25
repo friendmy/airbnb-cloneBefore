9e3b3606b24ebb582bccc40991396bacfe475b08
friendmy <friendmy@gmail.com>
2019-11-25 12:39:12 +0900

    room add

--- config/settings.py
+++ config/settings.py
@@ -45,9 +45,9 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = ["django_countries"]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
