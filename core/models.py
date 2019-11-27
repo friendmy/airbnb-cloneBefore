a20d7d459a64688d7bea2ef5f8452463a49b7101
friendmy <friendmy@gmail.com>
2019-11-28 08:51:11 +0900

    AdminPanel

--- core/models.py
+++ core/models.py
@@ -8,6 +8,5 @@ class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-
-class Meta:
-    abstract = True
+    class Meta:
+        abstract = True
